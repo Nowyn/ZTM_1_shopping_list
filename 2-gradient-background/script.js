6e8cbda950f615498c18c96773d12a266ac274fc
Elena D <danilova.elena.v@gmail.com>
2019-06-05 23:28:00 +0500

    Create script.js

---
+++ 2-gradient-background/script.js
@@ -0,0 +1,42 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomButton = document.querySelector(".random");
+
+function setGradient(color1, color2) {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1
+	+ ", "
+	+ color2
+	+ ")";
+
+	css.textContent = body.style.background + ";"
+}
+
+function setColors() {
+	setGradient(color1.value, color2.value);
+}
+
+function getRandomColor() {
+	var color = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
+	return color;
+}
+
+function setRandomGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ getRandomColor()
+	+ ", "
+	+ getRandomColor()
+	+ ")";
+
+	css.textContent = body.style.background + ";"
+}
+
+color1.addEventListener("input", setColors);
+
+color2.addEventListener("input", setColors);
+
+randomButton.addEventListener("click", setRandomGradient);
